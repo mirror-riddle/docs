a4c98bfc91c531d55b8d825643ebbb0552d01e86
hejin <hejinpy@163.com>
2020-03-06 10:54:02 +0800

    update publish gh page shell

--- website/publish-gh-pages.sh
+++ website/publish-gh-pages.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
+#! usr/bin/env bash
 
-export GIT_USER=mirror-riddle
-
-export CURRENT_BRANCH=master
-
-export USE_SSH=true
-
-yarn run publish-gh-pages
+GIT_USER=mirror-riddle CURRENT_BRANCH=master USE_SSH=true yarn run publish-gh-pages
